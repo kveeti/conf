6251c89cab3ad476cdb67edd9213f02379a85c95
Veeti K <veeti@veetik.com>
2024-01-18 07:53:50 +0200

    tune write remap

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -29,7 +29,7 @@ vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
 vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("n", "<leader>w", ":w<CR>")
+vim.keymap.set("n", "<leader>w", [[:w<CR>]], { noremap = true, silent = true })
 
 -- center cursor
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
