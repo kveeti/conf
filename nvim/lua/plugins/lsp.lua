7b2493da97eee5448f7180ebcd66aa11495f943d
Veeti K <veeti@veetik.com>
2024-02-16 19:54:39 +0200

    keymap stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,96 +1,77 @@
 return {
-	'neovim/nvim-lspconfig',
-	dependencies = {
-		-- Automatically install LSPs to stdpath for neovim
-		'williamboman/mason.nvim',
-		'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        { 'j-hui/fidget.nvim', opts = {} },
+        'folke/neodev.nvim',
+    },
+    config = function()
+        local on_attach = function(_, bufnr)
+            local nmap = function(keys, func, desc)
+                if desc then
+                    desc = 'LSP: ' .. desc
+                end
 
-		-- Useful status updates for LSP
-		{ 'j-hui/fidget.nvim', opts = {} },
+                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+            end
 
-		-- Additional lua configuration, makes nvim stuff amazing!
-		'folke/neodev.nvim',
-	},
-	config = function()
-		local on_attach = function(_, bufnr)
-			local nmap = function(keys, func, desc)
-				if desc then
-					desc = 'LSP: ' .. desc
-				end
+            nmap('<leader>r', vim.lsp.buf.rename, '[R]e[n]ame')
+            nmap('<leader>c', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-				vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-			end
+            nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+            nmap('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+            nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+            nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+            nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-			nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-			nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+            nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+            nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+        end
 
-			nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-			nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-			nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-			nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-			nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-			nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        require('mason').setup()
+        require('mason-lspconfig').setup()
 
-			-- See `:help K` for why this keymap
-			nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-			nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+        local servers = {
+            rust_analyzer         = {},
+            html                  = { filetypes = { 'html', 'twig', 'hbs' } },
+            lua_ls                = {
+                Lua = {
+                    workspace = { checkThirdParty = false },
+                    telemetry = { enable = false },
+                    diagnostics = { disable = { 'missing-fields' } },
+                },
+            },
+            cssls                 = {},
+            emmet_language_server = {},
+            eslint                = {},
+            tsserver              = {},
+            jsonls                = {},
+            tailwindcss           = {},
+            gopls                 = {},
+        }
 
-			-- Lesser used LSP functionality
-			nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-			nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-			nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-			nmap('<leader>wl', function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, '[W]orkspace [L]ist Folders')
-		end
+        require('neodev').setup()
 
-		-- mason-lspconfig requires that these setup functions are called in this order
-		-- before setting up the servers.
-		require('mason').setup()
-		require('mason-lspconfig').setup()
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-		local servers = {
-			rust_analyzer         = {},
-			html                  = { filetypes = { 'html', 'twig', 'hbs' } },
-			lua_ls                = {
-				Lua = {
-					workspace = { checkThirdParty = false },
-					telemetry = { enable = false },
-					diagnostics = { disable = { 'missing-fields' } },
-				},
-			},
-			cssls                 = {},
-			emmet_language_server = {},
-			eslint                = {},
-			tsserver              = {},
-			htmx                  = {},
-			jsonls                = {},
-			tailwindcss           = {},
-		}
+        local mason_lspconfig = require 'mason-lspconfig'
 
-		-- Setup neovim lua configuration
-		require('neodev').setup()
+        mason_lspconfig.setup {
+            ensure_installed = vim.tbl_keys(servers),
+        }
 
-		-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-		-- Ensure the servers above are installed
-		local mason_lspconfig = require 'mason-lspconfig'
-
-		mason_lspconfig.setup {
-			ensure_installed = vim.tbl_keys(servers),
-		}
-
-		mason_lspconfig.setup_handlers {
-			function(server_name)
-				require('lspconfig')[server_name].setup {
-					capabilities = capabilities,
-					on_attach = on_attach,
-					settings = servers[server_name],
-					filetypes = (servers[server_name] or {}).filetypes,
-				}
-			end,
-		}
-	end
+        mason_lspconfig.setup_handlers {
+            function(server_name)
+                require('lspconfig')[server_name].setup {
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                    settings = servers[server_name],
+                    filetypes = (servers[server_name] or {}).filetypes,
+                }
+            end,
+        }
+    end
 }
