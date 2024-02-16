4a76212fe39b90030d28eb685fa96928d1eba9c9
Veeti K <veeti@veetik.com>
2024-02-16 19:54:50 +0200

    options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,51 +1,32 @@
--- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
-
--- Relative line numbers
-vim.o.relativenumber = true
-
--- Enable mouse mode
 vim.o.mouse = 'a'
+vim.opt.guicursor = ""
 
--- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+vim.wo.number = true
+vim.o.relativenumber = true
+vim.wo.signcolumn = 'yes'
+vim.o.wrap = false
+
 vim.o.clipboard = 'unnamedplus'
 
--- Enable break indent
-vim.o.breakindent = true
 
--- Save undo history
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
--- Decrease update time
 vim.o.updatetime = 50
 vim.o.timeoutlen = 300
 
--- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
 
--- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
-vim.o.shiftwidth = 4
-vim.o.tabstop = 4
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.breakindent = true
+vim.o.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
--- Scroll off
 vim.o.scrolloff = 8
 
--- hidden
-vim.o.hidden = true
-
--- no swap
 vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
