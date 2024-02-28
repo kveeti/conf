6f13e084ff2fa659618e1d955e1bbd5e586b3387
Veeti K <veeti@veetik.com>
2024-02-28 20:15:23 +0200

    copilot

--- home-manager/nvim/lua/plugins/copilot.lua
+++ home-manager/nvim/lua/plugins/copilot.lua
@@ -3,5 +3,7 @@ return {
 	event = "TextChangedI",
 	config = function()
 		vim.g.copilot_assume_mapped = true;
+        vim.g.copilot_no_tab_map = true
+        vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 	end
 }
