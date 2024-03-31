9e8cea3c75d1fd3e6009a56da99f9e7f76f237e8
Veeti K <veeti@veetik.com>
2024-03-31 18:54:32 +0300

    arch2

---
+++ _arch/2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [ "$EUID" -e 0 ]; then
+    echo "this script is not meant to be run as root" 
+    exit 1
+fi
+
+# install aur helper
+cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ~
+rm -rf ./yay
+
+# aur packages
+yay -S \
+  tofi \
+  minecraft-launcher \
+  downgrade
