d7f17de267d00967153bea419ee6698670931cd6
Veeti K <veeti@veetik.com>
2024-07-03 18:29:03 +0300

    add disk encryption (#1)
    * disk encryption
    
    * more ram on qemu command

--- arch/0.sh
+++ arch/0.sh
@@ -13,33 +13,44 @@ pacman -Sy --noconfirm archlinux-keyring
 
 if [[ $DISK == /dev/nvme* ]]; then
     boot="${DISK}p1"
-    swap="${DISK}p2"
-    root="${DISK}p3"
+    rest="${DISK}p2"
 else
     boot="${DISK}1"
-    swap="${DISK}2"
-    root="${DISK}3"
+    rest="${DISK}2"
 fi
-
-config_set ROOT_PARTITION "${root}"
 
 sfdisk "${DISK}" <<EOF
 label: gpt
 unit: sectors
 
 ${boot} : start=2048,  size=1G,  type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
-${swap} : start=,      size=8G,  type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
-${root} : start=,      size=,    type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
+${rest} : start=,      size=,    type=4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
 EOF
 
+config_set REST_PARTITION "${rest}"
+
+modprobe dm-crypt
+
+cryptsetup luksFormat "${rest}"
+cryptsetup open --type luks "${rest}" cryptlvm
+
+pvcreate /dev/mapper/cryptlvm
+vgcreate vg /dev/mapper/cryptlvm
+
+lvcreate -L 8G vg -n swap
+lvcreate -l 100%FREE vg -n root
+
+mkfs.ext4 /dev/vg/root
+mount /dev/vg/root /mnt
+
+mkswap /dev/vg/swap
+swapon /dev/vg/swap
+
 mkfs.fat -F 32 "${boot}"
-mkswap "${swap}"
-mkfs.ext4 "${root}"
-
-mount "${root}" /mnt
 mount --mkdir "${boot}" /mnt/boot
-swapon "${swap}"
 
-pacstrap -K /mnt base linux-lts linux-firmware linux-headers sudo vim git curl
+pacstrap -K /mnt base linux-lts linux-firmware linux-headers sudo vim git curl lvm2
+
+genfstab -U /mnt >> /mnt/etc/fstab
 
 cp -R . /mnt/root/scripts
