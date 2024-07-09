6a6e887ad629bb3cec3aa88b88b7e278f1795338
Veeti K <veeti@veetik.com>
2024-07-09 22:46:20 +0300

    update mac

---
+++ mac/dots.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DOTS="${HOME}/.dots"
+rm -rf "${DOTS}"
+
+cd "${HOME}"
+
+git clone --branch dots_mac --single-branch --bare https://github.com/veeti-k/conf "${DOTS}"
+function dots {
+   /usr/bin/git --git-dir="${DOTS}" --work-tree="${HOME}" $@
+}
+dots checkout
+if [ $? = 0 ]; then
+	echo "checked out config";
+else
+	echo "backing up pre-existing files";
+	mkdir -p .config-backup
+	dots checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} bash -c 'rsync -R {} .config-backup/ && rm -r {}'
+fi;
+dots checkout
+dots config status.showUntrackedFiles no
