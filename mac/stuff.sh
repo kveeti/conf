857e1b116ae11a46aa1f3bddcf175dab200cca6f
Veeti K <veeti@veetik.com>
2024-12-28 22:01:55 +0200

    update mac config

--- mac/stuff.sh
+++ mac/stuff.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-brew install google-chrome firefox orbstack neovim ripgrep tmux alacritty gh keepassxc linearmouse rectangle font-jetbrains-mono-nerd-font
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-defaults write -g InitialKeyRepeat -int 10
+defaults write -g InitialKeyRepeat -int 9
 defaults write -g KeyRepeat -int 1
 
 source "./_dock_utils.sh"
-defaults read com.apple.Dock autohide
+defaults write com.apple.Dock autohide -bool true
 defaults write com.apple.dock tilesize -int 32;
 defaults write com.apple.dock largesize -integer 40
 defaults write com.apple.dock orientation left
@@ -18,3 +13,6 @@ clear_dock
 disable_recent_apps_from_dock
 killall Dock
 
+brew install google-chrome firefox brave-browser orbstack neovim ripgrep ghostty gpg gh keepassxc linearmouse rectangle font-jetbrains-mono-nerd-font
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
