6f4f74abd9b585e65976d7679f0deed3920b6ba0
Veeti K <veeti@veetik.com>
2025-12-24 05:36:59 +0200

    backup host init

---
+++ backup/host/config.nix
@@ -0,0 +1,273 @@
+{ config, pkgs, lib, keys, ... }:
+
+{
+  config.boot.loader.systemd-boot.enable = true;
+  config.boot.loader.efi.canTouchEfiVariables = true;
+
+  config.boot.kernelParams = [ "ip=dhcp" ];
+  config.boot.initrd = {
+    availableKernelModules = [ "e1000e" "igb" ];
+    network = {
+      enable = true;
+      ssh = {
+        enable = true;
+        port = 2222;
+        authorizedKeys = keys.admins;
+        hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
+        shell = "/bin/cryptsetup-askpass";
+      };
+    };
+  };
+
+  config.networking.hostName = "backup";
+  config.time.timeZone = "UTC";
+  config.i18n.defaultLocale = "en_US.UTF-8";
+  config.i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fi_FI.UTF-8";
+    LC_IDENTIFICATION = "fi_FI.UTF-8";
+    LC_MEASUREMENT = "fi_FI.UTF-8";
+    LC_MONETARY = "fi_FI.UTF-8";
+    LC_NAME = "fi_FI.UTF-8";
+    LC_NUMERIC = "fi_FI.UTF-8";
+    LC_PAPER = "fi_FI.UTF-8";
+    LC_TELEPHONE = "fi_FI.UTF-8";
+    LC_TIME = "fi_FI.UTF-8";
+  };
+  config.console.keyMap = "fi";
+  config.services.xserver.xkb = {
+    layout = "fi";
+    variant = "";
+  };
+
+  config.age.secrets.password.file = ./secrets/password.age;
+  config.users.users = {
+    root = {
+      openssh.authorizedKeys.keys = keys.admins;
+      hashedPasswordFile = config.age.secrets.password.path;
+    };
+
+    veeti = {
+      useDefaultShell = true;
+      createHome = true;
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = keys.admins;
+      hashedPasswordFile = config.age.secrets.password.path;
+    };
+
+    backup = {
+      useDefaultShell = true;
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = keys.all;
+      hashedPasswordFile = config.age.secrets.password.path;
+    };
+
+    runner = {
+      useDefaultShell = true;
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = keys.admins;
+      hashedPasswordFile = config.age.secrets.password.path;
+      linger = true;
+    };
+  };
+
+  config.systemd.tmpfiles.rules = [
+    "d /srv/docker 0755 runner users -"
+    "d /srv/backups 0755 backup users -"
+  ];
+
+  config.security.sudo.wheelNeedsPassword = false;
+
+  config.services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      PubkeyAuthentication = true;
+      ChallengeResponseAuthentication = false;
+      X11Forwarding = false;
+    };
+    hostKeys = [{
+      type = "ed25519";
+      path = "/etc/ssh/ssh_host_ed25519_key";
+    }];
+  };
+
+  config.networking.useNetworkd = true;
+  config.networking.firewall.enable = true;
+  config.networking.firewall.allowedTCPPorts = [
+    # ssh
+    22
+
+    # port redirect
+    80 443 8080 8443
+  ];
+
+  # redirect 80 -> 8080 and 443 -> 8443
+  # docker is rootless, it can only listen on ports >1024
+  config.boot.kernel.sysctl = {
+    "net.ipv4.conf.all.forwarding" = 1;
+  };
+  config.networking = {
+    firewall.extraCommands = ''
+      iptables -A PREROUTING -t nat -p TCP --dport 80 -j REDIRECT --to-port 8080
+      iptables -A PREROUTING -t nat -p TCP --dport 443 -j REDIRECT --to-port 8443
+    '';
+  };
+
+  config.environment.systemPackages = with pkgs; [
+    vim
+    git
+    btop
+    docker
+    docker-compose
+  ];
+
+  config.services.prometheus.exporters.node = {
+    enable = true;
+    port = 9100;
+    openFirewall = false;
+    enabledCollectors = [
+      "logind"
+      "systemd"
+    ];
+  };
+
+  config.age.secrets.alloy-env.file = ./secrets/alloy-env.age;
+  config.services.alloy = {
+    enable = true;
+    environmentFile = config.age.secrets.alloy-env.path;
+  };
+  config.environment.etc."alloy/config.alloy".text = ''
+    logging {
+      level = "info"
+    }
+
+    //
+    // LOGS
+    //
+    loki.source.journal "docker_logs" {
+      max_age       = "24h"
+      matches       = "_SYSTEMD_USER_UNIT=docker.service"
+      forward_to    = [loki.write.victorialogs.receiver]
+      relabel_rules = loki.relabel.docker_logs.rules
+    }
+
+    loki.relabel "docker_logs" {
+      // compose directory name
+      // eg: o11s
+      rule {
+        source_labels = ["__journal_com_docker_compose_project"]
+        target_label  = "compose_project"
+      }
+
+      // compose service name
+      // eg: traefik
+      rule {
+        source_labels = ["__journal_com_docker_compose_service"]
+        target_label  = "compose_service"
+      }
+
+      // container short unique id
+      // eg: 1991eab3968b
+      rule {
+        source_labels = ["__journal_container_id"]
+        target_label  = "container_id"
+      }
+
+      // container name
+      // with compose will be eg: o11s-traefik-1
+      //   where "o11s" is the compose project, "traefik" is the service name
+      //   and 1 is the instance count of that compose service
+      rule {
+        source_labels = ["__journal_container_name"]
+        target_label  = "container_name"
+      }
+
+      // image name with tag
+      // eg: docker.io/traefik@sha256:c8bcb479c8057a29b05b1f3a5dcfb580fa67bc6adc41e48eabb168512c6a8c8b
+      rule {
+        source_labels = ["__journal_image_name"]
+        target_label  = "image"
+      }
+
+      // journald default _hostname field
+      rule {
+        source_labels = ["__journal__hostname"]
+        target_label  = "host"
+      }
+
+      forward_to = []
+    }
+
+    loki.write "victorialogs" {
+      endpoint {
+        url = "https://o11s-logs.internal.veetik.com:8443/insert/loki/api/v1/push"
+
+        basic_auth {
+          username = env("LOGS_USER")
+          password = env("LOGS_PASS")
+        }
+      }
+    }
+    //
+    // LOGS
+    //
+
+
+    //
+    // METRICS
+    //
+    discovery.relabel "node_exporter" {
+      targets = [{
+        __address__ = "127.0.0.1:9100",
+      }]
+
+      rule {
+        replacement  = constants.hostname
+        target_label = "instance"
+      }
+    }
+
+    prometheus.scrape "node_exporter" {
+      targets    = discovery.relabel.node_exporter.output
+      forward_to = [prometheus.remote_write.victoriametrics.receiver]
+      
+      scrape_interval = "15s"
+      scrape_timeout  = "10s"
+    }
+
+    prometheus.remote_write "victoriametrics" {
+      endpoint {
+        url = "https://o11s-metrics.internal.veetik.com:8443/api/v1/write"
+
+        basic_auth {
+          username = env("METRICS_USER")
+          password = env("METRICS_PASS")
+        }
+      }
+    }
+    //
+    // METRICS
+    //
+  '';
+
+  config.virtualisation.docker = {
+    enable = false;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+      daemon.settings = {
+        dns = [ "1.1.1.1" "8.8.8.8" ];
+        data-root = "/srv/docker";
+        log-driver = "journald";
+        log-opts = {
+          labels = "com.docker.compose.service,com.docker.compose.project";
+        };
+      };
+    };
+  };
+
+  config.system.stateVersion = "25.05";
+}
