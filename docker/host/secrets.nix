02ad0ca073ca4f5a208163faab018ad9d5d383b2
Veeti K <veeti@veetik.com>
2026-02-01 18:35:47 +0200

    remove old configs

--- docker/host/secrets.nix
+++
@@ -1,6 +0,0 @@
-let
-  inherit (import ./keys.nix) all admins servu;
-in {
-  "secrets/password.age".publicKeys = [ servu ] ++ admins;
-  "secrets/alloy-env.age".publicKeys = [ servu ] ++ admins;
-}
--- servers/host/secrets.nix
+++
@@ -1,6 +0,0 @@
-let
-  inherit (import ./keys.nix) all admins servers;
-in {
-  "secrets/password.age".publicKeys = [ servers ] ++ admins;
-  "secrets/alloy-env.age".publicKeys = [ servers ] ++ admins;
-}
