6cea39510131710f1d4bb5f36b6d52c0146e31ab
Veeti K <veeti@veetik.com>
2026-04-02 17:51:18 +0300

    format and add tmux conf

--- mac/flake.nix
+++ mac/flake.nix
@@ -1,255 +1,263 @@
 {
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
 
-		nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
-		nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-		agenix = {
-			url = "github:ryantm/agenix";
-			inputs.nixpkgs.follows = "nixpkgs";
-			inputs.darwin.follows = "nix-darwin";
-		};
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.darwin.follows = "nix-darwin";
+    };
 
-		home-manager = {
-			url = "github:nix-community/home-manager/release-25.11";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-	};
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-	outputs = inputs@{ self, nix-darwin, nixpkgs, agenix, home-manager }: let
-		hostname = "e";
-		username = "veeti";
-		homeDir = "/Users/${username}";
-		system = "aarch64-darwin";
-		configuration = { pkgs, home, ... }: {
-			nix.settings.experimental-features = "nix-command flakes";
+  outputs = inputs@{ self, nix-darwin, nixpkgs, agenix, home-manager }:
+    let
+      hostname = "e";
+      username = "veeti";
+      homeDir = "/Users/${username}";
+      system = "aarch64-darwin";
+      configuration = { pkgs, home, ... }: {
+        nix.settings.experimental-features = "nix-command flakes";
 
+        networking.hostName = hostname;
 
-			networking.hostName = hostname;
+        environment.systemPackages = with pkgs; [
+          agenix.packages.${system}.agenix
+          vim
+          git
+          zstd
+          pv
+          eza
+          gnupg
+          fzf
+          home-manager
+          ripgrep
+          mpv
+          opencode
+        ];
 
-			environment.systemPackages = with pkgs; [
-				agenix.packages.${system}.agenix
-				vim
-				git
-				zstd
-				pv
-				eza
-				gnupg
-				fzf
-				home-manager
-				ripgrep
-				mpv
-				opencode
-			];
-			environment.variables = {
-				EDITOR = "nvim";
-				VISUAL = "nvim";
-			};
+        environment.variables = {
+          EDITOR = "nvim";
+          VISUAL = "nvim";
+        };
 
-			environment.shellAliases = {
-				nixswitch = "sudo darwin-rebuild switch --flake .#${hostname}";
-				ls = "eza -la";
-				f = "cd \"$(find ~/code ~/things -type d -maxdepth 7 -print0 | fzf --read0)\"";
-				gs = "git status --short";
-				gl = "git log --pretty=format:\"%C(yellow)%h%C(reset) %C(dim)%ad%C(reset) %C(green)%an%C(reset) %s\" --date=human";
-				gc = "git commit -S";
-				gca = "git commit -S --amend";
-				k = "kubectl";
-				e = "vim";
-			};
-			system.primaryUser = username;
-			users.users."${username}" = {
-				name = username;
-				home = homeDir;
-			};
+        environment.shellAliases = {
+          nixswitch = "sudo darwin-rebuild switch --flake .#${hostname}";
+          ls = "eza -la";
+          cd = "z";
+          f = "cd \"$(find ~/code ~/things -type d -maxdepth 7 -print0 | fzf --read0)\"";
+          gs = "git status --short";
+          gl = "git log --pretty=format:\"%C(yellow)%h%C(reset) %C(dim)%ad%C(reset) %C(green)%an%C(reset) %s\" --date=human";
+          gc = "git commit -S";
+          gca = "git commit -S --amend";
+          k = "kubectl";
+          e = "vim";
+        };
 
-			home-manager.useGlobalPkgs = true;
-			home-manager.useUserPackages = true;
-			home-manager.users."${username}".home = {
-				username = username;
-				homeDirectory = homeDir;
-				stateVersion = "25.05";
-			};
-			home-manager.sharedModules = [
-				({ config, lib, pkgs, ... }: {
-					programs.home-manager.enable = true;
-					programs.git.enable = true;
-					programs.git.settings = {
-						user.email = "veeti@veetik.com";
-						user.name = "Veeti K";
-						user.signingkey = "111E474490913E21";
-						commit.gpgsign = true;
-						branch.sort = "-committerdate";
-						push.autoSetupRemote = true;
-					};
-					programs.neovim = {
-						enable = true;
-						defaultEditor = true;
-						vimAlias = true;
-					};
+        system.primaryUser = username;
+        users.users."${username}" = {
+          name = username;
+          home = homeDir;
+        };
 
-					xdg.configFile."nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink ./nvim.conf;
-					xdg.configFile."ghostty/config".source = config.lib.file.mkOutOfStoreSymlink ./ghostty.conf;
-					xdg.configFile."ghostty/themes/vague".source = config.lib.file.mkOutOfStoreSymlink ./ghostty-vague.conf;
-				})
-			];
-			
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users."${username}".home = {
+          username = username;
+          homeDirectory = homeDir;
+          stateVersion = "25.05";
+        };
 
-			homebrew.enable = true;
-			homebrew.casks = [ "keepassxc" "ghostty" "brave-browser" "syncthing-app" ];
-			homebrew.brews = [ "lazygit" "colima" "tmux" ];
-			environment.systemPath = [ "/opt/homebrew/bin" ];
+        home-manager.sharedModules = [
+          ({ config, lib, pkgs, ... }: {
+            programs.home-manager.enable = true;
+            programs.git.enable = true;
+            programs.git.settings = {
+              user.email = "veeti@veetik.com";
+              user.name = "Veeti K";
+              user.signingkey = "111E474490913E21";
+              commit.gpgsign = true;
+              branch.sort = "-committerdate";
+              push.autoSetupRemote = true;
+            };
+            programs.neovim = {
+              enable = true;
+              defaultEditor = true;
+              vimAlias = true;
+            };
 
-			programs.zsh.enable = true;
-			programs.zsh.interactiveShellInit = ''
-                          enc() {
-                            local file="$1"
-                            if [[ -z "$${file}" ]]; then
-                                echo "usage: enc <file or dir>"
-                                return 1
-                            fi
-                        
-                            local passphrase1 passphrase2
-                            echo -n "enter passphrase: "
-                            read -s passphrase1
-                            echo
-                            echo -n "confirm passphrase: "
-                            read -s passphrase2
-                            echo
-                            if [[ "$passphrase1" != "$passphrase2" ]]; then
-                                echo "passphrases do not match. aborting."
-                                return 1
-                            fi
-                        
-                            tar -cf - "$file" | zstd -T0 | pv -c | gpg --no-symkey-cache --batch --yes --passphrase "$passphrase1" --symmetric --cipher-algo AES256 --compress-level 0 -o "$file.tar.zst.gpg"
-                            echo "done"
-                        }
-                        
-                        dec() {
-                            local file="$1"
-                            if [[ -z "$${file}" ]]; then
-                                echo "usage: dec <file.tar.zst.gpg>"
-                                return 1
-                            fi
-                        
-                            local tar_name=$(basename "$${file}" .tar.zst.gpg)
-                            if [[ -e "$${tar_name}" ]]; then
-                                echo "error: '$${tar_name}' already exists. aborting."
-                                return 1
-                            fi
-                        
-                            local passphrase
-                            echo -n "enter passphrase: "
-                            read -s passphrase
-                            echo
-                        
-                            gpg --no-symkey-cache --batch --passphrase "$passphrase" --decrypt "$file" | zstd -d | pv -c | tar -xf -
-                            echo "done"
-                        }
+            programs.zsh.enable = true;
+            programs.zsh.initContent = ''
+              enc() {
+                local file="$1"
+                if [[ -z "$file" ]]; then
+                  echo "usage: enc <file or dir>"
+                  return 1
+                fi
 
-                        function t() {
-                            if [[ $# -eq 1 ]]; then
-                                selected="$1"
-                            else
-                                selected=$(find ~/code -maxdepth 7 \( -name "node_modules" -o -name ".git" -o -name "dist" -o -name "build" -o -name "target" \) -prune -o -type d -print0 | fzf --read0)
-                            fi
-                            
-                            if [[ -z $selected ]]; then
-                                exit 0
-                            fi
-                            
-                            selected_name=$(basename "$selected" | tr . _)
-                            tmux_running=$(pgrep tmux)
-                            
-                            if [[ -z $TMUX ]] && [[ -z "$tmux_running" ]]; then
-                                tmux new-session -s "$selected_name" -c "$selected"
-                                exit 0
-                            fi
-                            
-                            if ! tmux has-session -t="$selected_name" 2> /dev/null; then
-                                tmux new-session -ds "$selected_name" -c "$selected"
-                            fi
-                            
-                            if [[ -z $TMUX ]]; then
-                                tmux attach -t "$selected_name"
-                            else
-                                tmux switch-client -t "$selected_name"
-                            fi
-                            }
-                        '';
+                local passphrase1 passphrase2
+                echo -n "enter passphrase: "
+                read -s passphrase1
+                echo
+                echo -n "confirm passphrase: "
+                read -s passphrase2
+                echo
+                if [[ "$passphrase1" != "$passphrase2" ]]; then
+                  echo "passphrases do not match. aborting."
+                  return 1
+                fi
 
-			security.pam.services.sudo_local = {
-				enable = true;
-				touchIdAuth = true;
-			};
+                tar -cf - "$file" | zstd -T0 | pv -c | gpg --no-symkey-cache --batch --yes --passphrase "$passphrase1" --symmetric --cipher-algo AES256 --compress-level 0 -o "$file.tar.zst.gpg"
+                echo "done"
+              }
 
-			system.defaults = {
-				NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
-				
-				NSGlobalDomain.AppleICUForce24HourTime = true;
-				NSGlobalDomain.AppleTemperatureUnit = "Celsius";
-				NSGlobalDomain.AppleMeasurementUnits = "Centimeters";
-				NSGlobalDomain.AppleMetricUnits = 1;
-				menuExtraClock.Show24Hour = true;
-				menuExtraClock.ShowSeconds = true;
+              dec() {
+                local file="$1"
+                if [[ -z "$file" ]]; then
+                  echo "usage: dec <file.tar.zst.gpg>"
+                  return 1
+                fi
 
-				controlcenter.BatteryShowPercentage = false;
-				controlcenter.Bluetooth = true;
-				dock = {
-					orientation = "right";
-					autohide = true;
-					showhidden = true;
-					show-recents = false;
-					mru-spaces = false;
-					tilesize = 34;
-					persistent-apps = [];
+                local tar_name=$(basename "$file" .tar.zst.gpg)
+                if [[ -e "$tar_name" ]]; then
+                  echo "error: '$tar_name' already exists. aborting."
+                  return 1
+                fi
 
-					wvous-tl-corner = 1;
-					wvous-tr-corner = 1;
-					wvous-bl-corner = 1;
-					wvous-br-corner = 1;
-				};
+                local passphrase
+                echo -n "enter passphrase: "
+                read -s passphrase
+                echo
 
-				LaunchServices.LSQuarantine = false;
-				CustomSystemPreferences."com.apple.screensaver" = {
-					askForPassword = 1;
-					askForPasswordDelay = 0;
-				};
-				loginwindow = {
-					DisableConsoleAccess = true;
-					GuestEnabled = false;
-				};
+                gpg --no-symkey-cache --batch --passphrase "$passphrase" --decrypt "$file" | zstd -d | pv -c | tar -xf -
+                echo "done"
+              }
 
-				CustomSystemPreferences."com.apple.AdLib" = {
-					allowApplePersonalizedAdvertising = false;
-					allowIdentifierForAdvertising = false;
-					forceLimitAdTracking = true;
-					personalizedAdsMigrated = false;
-				};
-			};
-			system.startup.chime = false;
+              function t() {
+                if [[ $# -eq 1 ]]; then
+                  selected="$1"
+                else
+                  selected=$(find ~/code -maxdepth 7 \( -name "node_modules" -o -name ".git" -o -name "dist" -o -name "build" -o -name "target" \) -prune -o -type d -print0 | fzf --read0)
+                fi
 
-			system.defaults.trackpad = {
-				Clicking = false;
-				Dragging = false;
-			};
-			system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
-			system.defaults.NSGlobalDomain.KeyRepeat = 1;
-			system.keyboard.enableKeyMapping = true;
-			system.keyboard.remapCapsLockToControl = true;
+                if [[ -z $selected ]]; then
+                  exit 0
+                fi
 
-			system.configurationRevision = self.rev or self.dirtyRev or null;
-			system.stateVersion = 6;
-			nixpkgs.hostPlatform = system;
-		};
-	in
-	{
-		darwinConfigurations."${hostname}" = nix-darwin.lib.darwinSystem {
-			modules = [
-				home-manager.darwinModules.home-manager
-				configuration
-			];
-		};
-	};
+                selected_name=$(basename "$selected" | tr . _)
+                tmux_running=$(pgrep tmux)
+
+                if [[ -z $TMUX ]] && [[ -z "$tmux_running" ]]; then
+                  tmux new-session -s "$selected_name" -c "$selected"
+                  exit 0
+                fi
+
+                if ! tmux has-session -t="$selected_name" 2> /dev/null; then
+                  tmux new-session -ds "$selected_name" -c "$selected"
+                fi
+
+                if [[ -z $TMUX ]]; then
+                  tmux attach -t "$selected_name"
+                else
+                  tmux switch-client -t "$selected_name"
+                fi
+              }
+            '';
+
+            programs.zoxide = {
+              enable = true;
+              enableZshIntegration = true;
+            };
+
+            xdg.configFile."nvim/init.lua".source = config.lib.file.mkOutOfStoreSymlink ./nvim.conf;
+            xdg.configFile."ghostty/config".source = config.lib.file.mkOutOfStoreSymlink ./ghostty.conf;
+            xdg.configFile."ghostty/themes/vague".source = config.lib.file.mkOutOfStoreSymlink ./ghostty-vague.conf;
+            xdg.configFile."tmux/tmux.conf".source = config.lib.file.mkOutOfStoreSymlink ./tmux.conf;
+          })
+        ];
+
+        homebrew.enable = true;
+        homebrew.casks = [ "keepassxc" "ghostty" "brave-browser" "syncthing-app" ];
+        homebrew.brews = [ "lazygit" "colima" "tmux" ];
+        environment.systemPath = [ "/opt/homebrew/bin" ];
+
+        security.pam.services.sudo_local = {
+          enable = true;
+          touchIdAuth = true;
+        };
+
+        system.defaults = {
+          NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+          NSGlobalDomain.AppleICUForce24HourTime = true;
+          NSGlobalDomain.AppleTemperatureUnit = "Celsius";
+          NSGlobalDomain.AppleMeasurementUnits = "Centimeters";
+          NSGlobalDomain.AppleMetricUnits = 1;
+          menuExtraClock.Show24Hour = true;
+          menuExtraClock.ShowSeconds = true;
+
+          controlcenter.BatteryShowPercentage = false;
+          controlcenter.Bluetooth = true;
+          dock = {
+            orientation = "right";
+            autohide = true;
+            showhidden = true;
+            show-recents = false;
+            mru-spaces = false;
+            tilesize = 34;
+            persistent-apps = [];
+
+            wvous-tl-corner = 1;
+            wvous-tr-corner = 1;
+            wvous-bl-corner = 1;
+            wvous-br-corner = 1;
+          };
+
+          LaunchServices.LSQuarantine = false;
+          CustomSystemPreferences."com.apple.screensaver" = {
+            askForPassword = 1;
+            askForPasswordDelay = 0;
+          };
+          loginwindow = {
+            DisableConsoleAccess = true;
+            GuestEnabled = false;
+          };
+
+          CustomSystemPreferences."com.apple.AdLib" = {
+            allowApplePersonalizedAdvertising = false;
+            allowIdentifierForAdvertising = false;
+            forceLimitAdTracking = true;
+            personalizedAdsMigrated = false;
+          };
+        };
+
+        system.startup.chime = false;
+        system.defaults.trackpad = {
+          Clicking = false;
+          Dragging = false;
+        };
+        system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
+        system.defaults.NSGlobalDomain.KeyRepeat = 1;
+        system.keyboard.enableKeyMapping = true;
+        system.keyboard.remapCapsLockToControl = true;
+
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+        system.stateVersion = 6;
+        nixpkgs.hostPlatform = system;
+      };
+    in
+    {
+      darwinConfigurations."${hostname}" = nix-darwin.lib.darwinSystem {
+        modules = [
+          home-manager.darwinModules.home-manager
+          configuration
+        ];
+      };
+    };
 }
