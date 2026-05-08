2ec0a5fb215038c16d9de9ea70ba7f77a64c90ea
Veeti K <veeti@veetik.com>
2026-05-08 11:59:28 +0300

    i like my root to myself

--- nix-services-internal/host/config.nix
+++ nix-services-internal/host/config.nix
@@ -18,6 +18,15 @@
   config.nix.settings.experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
 
   config.boot.kernelParams = [ "ip=dhcp" ];
+  config.boot.blacklistedKernelModules = [ "esp4" "esp6" "rxrpc" ];
+  config.boot.extraModprobeConfig = ''
+    install esp4 /bin/false
+    install esp6 /bin/false
+    install rxrpc /bin/false
+  '';
+  config.system.activationScripts.unloadDirtyfragModules.text = ''
+    ${pkgs.kmod}/bin/rmmod esp4 esp6 rxrpc 2>/dev/null || true
+  '';
   config.boot.initrd = {
     availableKernelModules = [ "e1000e" "igb" ];
     network = {
@@ -228,4 +237,3 @@
 
   config.system.stateVersion = "25.11";
 }
-
