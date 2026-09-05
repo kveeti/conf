d9b738f5e707a6f3e739f429ea4d69e9d2d8f8bf
Veeti K <veeti@veetik.com>
2026-09-05 19:02:50 +0300

    keep LUKS unlock prompts active

--- atx/disk.nix
+++ atx/disk.nix
@@ -22,7 +22,10 @@
             content = {
               type = "luks";
               name = "cryptroot";
-              settings.allowDiscards = true;
+              settings = {
+                allowDiscards = true;
+                crypttabExtraOpts = [ "timeout=0" "tries=0" ];
+              };
               content = {
                 type = "zfs";
                 pool = "rpool";
